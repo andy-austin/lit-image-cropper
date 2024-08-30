c566bc42a5023223ccba1d0784aa6258e1c4d45f
andyfernandez <afgarabote@gmail.com>
2024-08-30 20:50:37 -0300

    adding root-component for demo purposes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import { LitElement, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+import './components/cropper';
+
+const DEMO_IMAGE_URL = 'https://images.unsplash.com/photo-1438761681033-6461ffad8d80';
+
+@customElement('root-component')
+export class RootComponent extends LitElement {
+  render() {
+    return html`<lit-image-cropper src=${DEMO_IMAGE_URL}></lit-image-cropper>`;
+  }
+}
+
 export { LitImageCropper } from './components/cropper';
